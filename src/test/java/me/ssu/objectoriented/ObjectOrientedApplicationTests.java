7d5dcbe220980952558463c82d02e7f5ee26f9c8
김수환 <zzanggoon8@hiconsy.com>
2023-03-29 10:10:50 +0900

    first commit

---
+++ src/test/java/me/ssu/objectoriented/ObjectOrientedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.objectoriented;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectOrientedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
